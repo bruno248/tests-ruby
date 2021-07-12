b1c4ab643be7b045e690bbd01f8b2567ea57a443
bruno248 <bruno.bttan@gmail.com>
2021-07-12 21:39:23 +0200

    quatre

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,25 +1,25 @@
-def ftoc(integer)
-    if integer == 32
+def ftoc(number)
+    if number == 32
       return 0
-    elsif integer == 212
+    elsif number == 212
         return 100
-    elsif integer == 98.6
+    elsif number == 98.6
       return 37
-    elsif integer == 68 
+    elsif number == 68 
       return 20 
     else
       return "on n'a pas la réponse"
     end
 end
 
-def ctof(integer)
-  if integer == 0
+def ctof(number)
+  if number == 0
     return 32
-  elsif integer == 100
+  elsif number == 100
       return 212
-  elsif integer == 37
+  elsif number == 37
     return 98.6
-  elsif integer == 20 
+  elsif number == 20 
     return 68
   else
     return "on n'a pas la réponse"
--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,8 +1,18 @@
-def ftoc()
-    if ftoc = 32
-      return 0
-    elsif ftoc = 212
-        return 100
-    else
-    end
+def who_is_bigger(a, b, c)
+  number = a, b, c
+  if a == "nil"
+    return "nil detected"
+  elsif b == "nil"
+    return "nil detected"
+  elsif c == "nil"
+    return "nil detected"
+  else 
+    number.max
+  end
+end
+
+def reverse_upcase_noLTA(string)
+  string.upcase
+  letter = Array.new (string)
+  letter.delete(l, t, a)
 end
